ddcf8231d16a94d63107d8c9fe8e97ab051f4a30
StrikeSNC <40461728+StrikeSNC@users.noreply.github.com>
2021-08-19 16:22:34 +0800

    Model Enhancement

--- src/main/java/mtr/block/BlockTactileMap.java
+++ src/main/java/mtr/block/BlockTactileMap.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
@@ -20,7 +21,8 @@ import net.minecraft.world.World;
 import java.util.stream.Stream;
 
 public class BlockTactileMap extends HorizontalFacingBlock {
-        public BlockTactileMap() {
+
+    public BlockTactileMap() {
             super(FabricBlockSettings.of(Material.METAL, MapColor.OFF_WHITE).requiresTool().hardness(2));
         }
 
@@ -34,17 +36,18 @@ public class BlockTactileMap extends HorizontalFacingBlock {
             builder.add(FACING);
         }
 
-        public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-            Stream.of(
-                    VoxelShapes.cuboid(2, 1, 7, 14, 2, 17),
-                    VoxelShapes.cuboid(6, 1, 7, 9, 9, 8),
-                    VoxelShapes.cuboid(4, 0, 6, 12, 1, 9)
-            ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR);});
-            return VoxelShapes.cuboid(0.75f, 0f, 0.15f, 1.0f, 1.0f, 0.85f);
-        }
-    public ActionResult onUse(BlockState blockState, World world, BlockPos blockPos, PlayerEntity placedBy, Hand hand, BlockHitResult blockHitResult) {
+       // public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+         //   Stream.of(
+         //           VoxelShapes.cuboid(3, 0, 6, 12, 1, 9),
+         //           VoxelShapes.cuboid(6, 1, 7, 9, 9.5, 8),
+         //           VoxelShapes.cuboid(1.5, 1, 7, 13.5, 2, 17)
+         //   ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR);});
+       //     return VoxelShapes.cuboid(0.75f, 0f, 0.15f, 1.0f, 1.0f, 0.85f);
+       // }
+    public ActionResult onUse(BlockState blockState, World world, BlockPos blockPos, PlayerEntity placedBy,Hand hand, BlockHitResult blockHitResult) {
         if (!world.isClient) {
             world.playSound(null, blockPos, MTR.TACTILE_MAP_MUSIC, SoundCategory.BLOCKS, 1f, 1f);
+            player.sendMessage(new TranslatableText("Right click again to stop playback"), true);
         }
 
         return ActionResult.success(false);
