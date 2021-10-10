1bf55a546a1bc3903d1dcb84829072d6d76f113c
Jonathan Ho <jonafanho@gmail.com>
2021-10-10 14:13:19 -0700

    Loading Blockbench models via resource packs

--- src/main/java/mtr/config/CustomResources.java
+++ src/main/java/mtr/config/CustomResources.java
@@ -5,8 +5,10 @@ import com.google.gson.JsonParser;
 import mtr.MTR;
 import mtr.data.TrainRegistry;
 import mtr.gui.DashboardScreen;
+import mtr.model.TrainModelRegistry;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.resource.ResourceManager;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 import java.io.IOException;
@@ -15,6 +17,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class CustomResources implements SimpleSynchronousResourceReloadListener {
 
@@ -31,7 +35,14 @@ public class CustomResources implements SimpleSynchronousResourceReloadListener 
 	private static final String CUSTOM_TRAINS_NAME = "name";
 	private static final String CUSTOM_TRAINS_COLOR = "color";
 	private static final String CUSTOM_TRAINS_BASE_TRAIN_TYPE = "base_train_type";
+	private static final String CUSTOM_TRAINS_MODEL = "model";
+	private static final String CUSTOM_TRAINS_MODEL_PROPERTIES = "model_properties";
 	private static final String CUSTOM_TRAINS_TEXTURE_ID = "texture_id";
+
+	private static final String CUSTOM_TRAINS_MODEL_PROPERTIES_LENGTH = "length";
+	private static final String CUSTOM_TRAINS_MODEL_PROPERTIES_WIDTH = "width";
+	private static final String CUSTOM_TRAINS_MODEL_PROPERTIES_HAS_CONNECTION = "has_connection";
+	private static final String CUSTOM_TRAINS_MODEL_PROPERTIES_SOUND_COUNT = "sound_count";
 
 	private static final String CUSTOM_SIGNS_TEXTURE_ID = "texture_id";
 	private static final String CUSTOM_SIGNS_FLIP_TEXTURE = "flip_texture";
@@ -39,6 +50,11 @@ public class CustomResources implements SimpleSynchronousResourceReloadListener 
 	private static final String CUSTOM_SIGNS_FLIP_CUSTOM_TEXT = "flip_custom_text";
 	private static final String CUSTOM_SIGNS_SMALL = "small";
 	private static final String CUSTOM_SIGNS_BACKGROUND_COLOR = "background_color";
+
+	private static final String SOUND_ACCELERATION = "_acceleration_";
+	private static final String SOUND_DECELERATION = "_deceleration_";
+	private static final String SOUND_DOOR_OPEN = "_door_open";
+	private static final String SOUND_DOOR_CLOSE = "_door_close";
 
 	@Override
 	public Identifier getFabricId() {
@@ -50,84 +66,109 @@ public class CustomResources implements SimpleSynchronousResourceReloadListener 
 		customTrains.clear();
 		customSigns.clear();
 
+		readResource(manager, MTR.MOD_ID + ":" + CUSTOM_RESOURCES_ID + ".json", jsonConfig -> {
+			try {
+				jsonConfig.get(CUSTOM_TRAINS_KEY).getAsJsonObject().entrySet().forEach(entry -> {
+					try {
+						final JsonObject jsonObject = entry.getValue().getAsJsonObject();
+
+						final int color;
+						if (jsonObject.has(CUSTOM_TRAINS_COLOR)) {
+							color = DashboardScreen.colorStringToInt(jsonObject.get(CUSTOM_TRAINS_COLOR).getAsString());
+						} else {
+							color = 0;
+						}
+
+						if (jsonObject.has(CUSTOM_TRAINS_BASE_TRAIN_TYPE)) {
+							final TrainRegistry.TrainType trainType = TrainRegistry.getTrainType(jsonObject.get(CUSTOM_TRAINS_BASE_TRAIN_TYPE).getAsString());
+							customTrains.put(CUSTOM_TRAIN_ID_PREFIX + entry.getKey(), new CustomTrain(jsonObject.get(CUSTOM_TRAINS_NAME).getAsString(), color, trainType, jsonObject.get(CUSTOM_TRAINS_TEXTURE_ID).getAsString()));
+						} else if (jsonObject.has(CUSTOM_TRAINS_MODEL) && jsonObject.has(CUSTOM_TRAINS_MODEL_PROPERTIES)) {
+							readResource(manager, jsonObject.get(CUSTOM_TRAINS_MODEL).getAsString(), jsonModel -> readResource(manager, jsonObject.get(CUSTOM_TRAINS_MODEL_PROPERTIES).getAsString(), jsonProperties -> {
+								final int length = jsonProperties.get(CUSTOM_TRAINS_MODEL_PROPERTIES_LENGTH).getAsInt();
+								final int width = jsonProperties.get(CUSTOM_TRAINS_MODEL_PROPERTIES_WIDTH).getAsInt();
+								final boolean hasConnection = jsonProperties.get(CUSTOM_TRAINS_MODEL_PROPERTIES_HAS_CONNECTION).getAsBoolean();
+								final int soundCount = jsonProperties.get(CUSTOM_TRAINS_MODEL_PROPERTIES_SOUND_COUNT).getAsInt();
+
+								final SoundEvent[] accelerationSoundEvents = registerSoundEvents(soundCount, 3, entry.getKey() + SOUND_ACCELERATION);
+								final SoundEvent[] decelerationSoundEvents = registerSoundEvents(soundCount, 3, entry.getKey() + SOUND_DECELERATION);
+								final SoundEvent doorOpenSoundEvent = new SoundEvent(new Identifier(entry.getKey() + SOUND_DOOR_OPEN));
+								final SoundEvent doorCloseSoundEvent = new SoundEvent(new Identifier(entry.getKey() + SOUND_DOOR_CLOSE));
+
+								TrainRegistry.register(color, length, -1, width, hasConnection, accelerationSoundEvents, decelerationSoundEvents, doorOpenSoundEvent, doorCloseSoundEvent, 0.5F, false, entry.getKey());
+								TrainModelRegistry.register(entry.getKey(), new DynamicTrainModel(jsonModel, jsonProperties));
+								System.out.println("Registered train id " + entry.getKey());
+							}));
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+			} catch (Exception ignored) {
+			}
+
+			try {
+				jsonConfig.get(CUSTOM_SIGNS_KEY).getAsJsonObject().entrySet().forEach(entry -> {
+					try {
+						final JsonObject jsonObject = entry.getValue().getAsJsonObject();
+
+						final boolean flipTexture;
+						if (jsonObject.has(CUSTOM_SIGNS_FLIP_TEXTURE)) {
+							flipTexture = jsonObject.get(CUSTOM_SIGNS_FLIP_TEXTURE).getAsBoolean();
+						} else {
+							flipTexture = false;
+						}
+
+						final String customText;
+						if (jsonObject.has(CUSTOM_SIGNS_CUSTOM_TEXT)) {
+							customText = jsonObject.get(CUSTOM_SIGNS_CUSTOM_TEXT).getAsString();
+						} else {
+							customText = "";
+						}
+
+						final boolean flipCustomText;
+						if (jsonObject.has(CUSTOM_SIGNS_FLIP_CUSTOM_TEXT)) {
+							flipCustomText = jsonObject.get(CUSTOM_SIGNS_FLIP_CUSTOM_TEXT).getAsBoolean();
+						} else {
+							flipCustomText = false;
+						}
+
+						final boolean small;
+						if (jsonObject.has(CUSTOM_SIGNS_SMALL)) {
+							small = jsonObject.get(CUSTOM_SIGNS_SMALL).getAsBoolean();
+						} else {
+							small = false;
+						}
+
+						final int backgroundColor;
+						if (jsonObject.has(CUSTOM_SIGNS_BACKGROUND_COLOR)) {
+							backgroundColor = DashboardScreen.colorStringToInt(jsonObject.get(CUSTOM_SIGNS_BACKGROUND_COLOR).getAsString());
+						} else {
+							backgroundColor = 0;
+						}
+
+						customSigns.put(CUSTOM_SIGN_ID_PREFIX + entry.getKey(), new CustomSign(new Identifier(jsonObject.get(CUSTOM_SIGNS_TEXTURE_ID).getAsString()), flipTexture, customText, flipCustomText, small, backgroundColor));
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+			} catch (Exception ignored) {
+			}
+		});
+
+		System.out.println("Loaded " + customTrains.size() + " custom train(s)");
+		customTrains.keySet().forEach(System.out::println);
+		System.out.println("Loaded " + customSigns.size() + " custom sign(s)");
+		customSigns.keySet().forEach(System.out::println);
+	}
+
+	private static void readResource(ResourceManager manager, String path, Consumer<JsonObject> callback) {
 		try {
-			manager.getAllResources(new Identifier(MTR.MOD_ID, CUSTOM_RESOURCES_ID + ".json")).forEach(resource -> {
+			manager.getAllResources(new Identifier(path)).forEach(resource -> {
 				try (final InputStream stream = resource.getInputStream()) {
-					final JsonObject jsonConfig = new JsonParser().parse(new InputStreamReader(stream, StandardCharsets.UTF_8)).getAsJsonObject();
-
-					try {
-						jsonConfig.get(CUSTOM_TRAINS_KEY).getAsJsonObject().entrySet().forEach(entry -> {
-							try {
-								final JsonObject jsonObject = entry.getValue().getAsJsonObject();
-
-								final int color;
-								if (jsonObject.has(CUSTOM_TRAINS_COLOR)) {
-									color = DashboardScreen.colorStringToInt(jsonObject.get(CUSTOM_TRAINS_COLOR).getAsString());
-								} else {
-									color = 0;
-								}
-
-								final TrainRegistry.TrainType trainType = TrainRegistry.getTrainType(jsonObject.get(CUSTOM_TRAINS_BASE_TRAIN_TYPE).getAsString());
-
-								customTrains.put(CUSTOM_TRAIN_ID_PREFIX + entry.getKey(), new CustomTrain(jsonObject.get(CUSTOM_TRAINS_NAME).getAsString(), color, trainType, jsonObject.get(CUSTOM_TRAINS_TEXTURE_ID).getAsString()));
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						});
-					} catch (Exception ignored) {
-					}
-
-					try {
-						jsonConfig.get(CUSTOM_SIGNS_KEY).getAsJsonObject().entrySet().forEach(entry -> {
-							try {
-								final JsonObject jsonObject = entry.getValue().getAsJsonObject();
-
-								final boolean flipTexture;
-								if (jsonObject.has(CUSTOM_SIGNS_FLIP_TEXTURE)) {
-									flipTexture = jsonObject.get(CUSTOM_SIGNS_FLIP_TEXTURE).getAsBoolean();
-								} else {
-									flipTexture = false;
-								}
-
-								final String customText;
-								if (jsonObject.has(CUSTOM_SIGNS_CUSTOM_TEXT)) {
-									customText = jsonObject.get(CUSTOM_SIGNS_CUSTOM_TEXT).getAsString();
-								} else {
-									customText = "";
-								}
-
-								final boolean flipCustomText;
-								if (jsonObject.has(CUSTOM_SIGNS_FLIP_CUSTOM_TEXT)) {
-									flipCustomText = jsonObject.get(CUSTOM_SIGNS_FLIP_CUSTOM_TEXT).getAsBoolean();
-								} else {
-									flipCustomText = false;
-								}
-
-								final boolean small;
-								if (jsonObject.has(CUSTOM_SIGNS_SMALL)) {
-									small = jsonObject.get(CUSTOM_SIGNS_SMALL).getAsBoolean();
-								} else {
-									small = false;
-								}
-
-								final int backgroundColor;
-								if (jsonObject.has(CUSTOM_SIGNS_BACKGROUND_COLOR)) {
-									backgroundColor = DashboardScreen.colorStringToInt(jsonObject.get(CUSTOM_SIGNS_BACKGROUND_COLOR).getAsString());
-								} else {
-									backgroundColor = 0;
-								}
-
-								customSigns.put(CUSTOM_SIGN_ID_PREFIX + entry.getKey(), new CustomSign(new Identifier(jsonObject.get(CUSTOM_SIGNS_TEXTURE_ID).getAsString()), flipTexture, customText, flipCustomText, small, backgroundColor));
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						});
-					} catch (Exception ignored) {
-					}
+					callback.accept(new JsonParser().parse(new InputStreamReader(stream, StandardCharsets.UTF_8)).getAsJsonObject());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-
 				try {
 					resource.close();
 				} catch (IOException e) {
@@ -136,11 +177,27 @@ public class CustomResources implements SimpleSynchronousResourceReloadListener 
 			});
 		} catch (Exception ignored) {
 		}
+	}
 
-		System.out.println("Loaded " + customTrains.size() + " custom train(s)");
-		customTrains.keySet().forEach(System.out::println);
-		System.out.println("Loaded " + customSigns.size() + " custom sign(s)");
-		customSigns.keySet().forEach(System.out::println);
+	private static SoundEvent[] registerSoundEvents(int size, int groupSize, String path) {
+		return IntStream.range(0, size).mapToObj(i -> {
+			String group;
+			switch (i % groupSize) {
+				case 0:
+					group = "a";
+					break;
+				case 1:
+					group = "b";
+					break;
+				case 2:
+					group = "c";
+					break;
+				default:
+					group = "";
+					break;
+			}
+			return new SoundEvent(new Identifier(path + (i / 3) + group));
+		}).toArray(SoundEvent[]::new);
 	}
 
 	public static class CustomTrain {
---
+++ src/main/java/mtr/config/DynamicTrainModel.java
@@ -0,0 +1,216 @@
+package mtr.config;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import mtr.model.ModelDoorOverlay;
+import mtr.model.ModelDoorOverlayTopBase;
+import mtr.model.ModelTrainBase;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class DynamicTrainModel extends ModelTrainBase {
+
+	private final Map<String, ModelPart> parts = new HashMap<>();
+	private final JsonObject properties;
+	private final int doorMax;
+
+	public DynamicTrainModel(JsonObject model, JsonObject properties) {
+		final JsonObject resolution = model.getAsJsonObject("resolution");
+		textureWidth = resolution.get("width").getAsInt();
+		textureHeight = resolution.get("height").getAsInt();
+
+		final Map<String, ModelPart> elementsByKey = new HashMap<>();
+		model.getAsJsonArray("elements").forEach(element -> {
+			final JsonObject elementObject = element.getAsJsonObject();
+			final ModelPart child = new ModelPart(this);
+
+			final Double[] origin = {0D, 0D, 0D};
+			getArrayFromValue(origin, elementObject, "origin", JsonElement::getAsDouble);
+			child.setPivot(-origin[0].floatValue(), -origin[1].floatValue(), origin[2].floatValue());
+
+			final Double[] rotation = {0D, 0D, 0D};
+			getArrayFromValue(rotation, elementObject, "rotation", JsonElement::getAsDouble);
+			setRotationAngle(child, -(float) Math.toRadians(rotation[0]), -(float) Math.toRadians(rotation[1]), (float) Math.toRadians(rotation[2]));
+
+			final Integer[] uvOffset = {0, 0};
+			getArrayFromValue(uvOffset, elementObject, "uv_offset", JsonElement::getAsInt);
+
+			final Double[] posFrom = {0D, 0D, 0D};
+			getArrayFromValue(posFrom, elementObject, "from", JsonElement::getAsDouble);
+			final Double[] posTo = {0D, 0D, 0D};
+			getArrayFromValue(posTo, elementObject, "to", JsonElement::getAsDouble);
+
+			final double inflate = elementObject.has("inflate") ? elementObject.get("inflate").getAsDouble() : 0;
+			final boolean mirror = elementObject.has("shade") && !elementObject.get("shade").getAsBoolean();
+
+			child.setTextureOffset(uvOffset[0], uvOffset[1]).addCuboid(
+					origin[0].floatValue() - posTo[0].floatValue(), origin[1].floatValue() - posTo[1].floatValue(), posFrom[2].floatValue() - origin[2].floatValue(),
+					posTo[0].floatValue() - posFrom[0].floatValue(), posTo[1].floatValue() - posFrom[1].floatValue(), posTo[2].floatValue() - posFrom[2].floatValue(),
+					(float) inflate, mirror
+			);
+			elementsByKey.put(elementObject.get("uuid").getAsString(), child);
+		});
+
+		model.getAsJsonArray("outliner").forEach(element -> {
+			final JsonObject elementObject = element.getAsJsonObject();
+			parts.put(elementObject.get("name").getAsString(), addChildren(elementObject, elementsByKey));
+		});
+
+		this.properties = properties;
+		doorMax = properties.get("door_max").getAsInt();
+	}
+
+	@Override
+	protected void renderWindowPositions(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails, boolean isEnd1Head, boolean isEnd2Head) {
+	}
+
+	@Override
+	protected void renderDoorPositions(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails, float doorLeftX, float doorRightX, float doorLeftZ, float doorRightZ, boolean isEnd1Head, boolean isEnd2Head) {
+		renderParts("parts_normal", matrices, vertices, renderStage, light, renderDetails, doorLeftZ, doorRightZ);
+		if (doorLeftZ > 0 || doorRightZ > 0) {
+			renderParts("parts_door_opened", matrices, vertices, renderStage, light, renderDetails, doorLeftZ, doorRightZ);
+		} else {
+			renderParts("parts_door_closed", matrices, vertices, renderStage, light, renderDetails, doorLeftZ, doorRightZ);
+		}
+	}
+
+	@Override
+	protected void renderHeadPosition1(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails, boolean useHeadlights) {
+		renderParts("parts_head_1", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		if (useHeadlights) {
+			renderParts("parts_head_1_headlights", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		} else {
+			renderParts("parts_head_1_tail_lights", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		}
+	}
+
+	@Override
+	protected void renderHeadPosition2(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails, boolean useHeadlights) {
+		renderParts("parts_head_2", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		if (useHeadlights) {
+			renderParts("parts_head_2_headlights", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		} else {
+			renderParts("parts_head_2_tail_lights", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+		}
+	}
+
+	@Override
+	protected void renderEndPosition1(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails) {
+		renderParts("parts_end_1", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+	}
+
+	@Override
+	protected void renderEndPosition2(MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, int position, boolean renderDetails) {
+		renderParts("parts_end_2", matrices, vertices, renderStage, light, renderDetails, 0, 0);
+	}
+
+	@Override
+	protected ModelDoorOverlay getModelDoorOverlay() {
+		return null;
+	}
+
+	@Override
+	protected ModelDoorOverlayTopBase getModelDoorOverlayTop() {
+		return null;
+	}
+
+	@Override
+	protected int[] getWindowPositions() {
+		return new int[]{0};
+	}
+
+	@Override
+	protected int[] getDoorPositions() {
+		return new int[]{0};
+	}
+
+	@Override
+	protected int[] getEndPositions() {
+		return new int[]{0, 0};
+	}
+
+	@Override
+	protected float getDoorAnimationX(float value, boolean opening) {
+		return 0;
+	}
+
+	@Override
+	protected float getDoorAnimationZ(float value, boolean opening) {
+		return smoothEnds(0, doorMax, 0, 0.5F, value);
+	}
+
+	private ModelPart addChildren(JsonObject jsonObject, Map<String, ModelPart> children) {
+		final ModelPart part = new ModelPart(this);
+		part.setPivot(0, 0, 0);
+		part.setTextureOffset(0, 0).addCuboid(0, 0, 0, 0, 0, 0);
+		jsonObject.getAsJsonArray("children").forEach(child -> part.addChild(child.isJsonObject() ? addChildren(child.getAsJsonObject(), children) : children.get(child.getAsString())));
+		return part;
+	}
+
+	private void renderParts(String category, MatrixStack matrices, VertexConsumer vertices, RenderStage renderStage, int light, boolean renderDetails, float doorLeftZ, float doorRightZ) {
+		properties.getAsJsonArray(category).forEach(partElement -> {
+			final JsonObject partObject = partElement.getAsJsonObject();
+			final boolean shouldRender = renderDetails || !partObject.has("skip_rendering_if_too_far") || !partObject.get("skip_rendering_if_too_far").getAsBoolean();
+
+			if (shouldRender && renderStage.toString().equals(partObject.get("stage").getAsString().toUpperCase())) {
+				final ModelPart part = parts.get(partObject.get("part_name").getAsString());
+
+				if (part != null) {
+					final float zOffset;
+					if (partObject.has("door_offset_z")) {
+						switch (partObject.get("door_offset_z").getAsString()) {
+							case "left":
+								zOffset = doorLeftZ;
+								break;
+							case "right":
+								zOffset = doorRightZ;
+								break;
+							case "left_negative":
+								zOffset = -doorLeftZ;
+								break;
+							case "right_negative":
+								zOffset = -doorRightZ;
+								break;
+							default:
+								zOffset = 0;
+								break;
+						}
+					} else {
+						zOffset = 0;
+					}
+
+					if (partObject.has("positions")) {
+						partObject.getAsJsonArray("positions").forEach(positionElement -> {
+							final float x = positionElement.getAsJsonArray().get(0).getAsFloat();
+							final float z = positionElement.getAsJsonArray().get(1).getAsFloat();
+							renderOnce(part, matrices, vertices, light, x, z + zOffset);
+						});
+					}
+
+					if (partObject.has("positions_flipped")) {
+						partObject.getAsJsonArray("positions_flipped").forEach(positionElement -> {
+							final float x = positionElement.getAsJsonArray().get(0).getAsFloat();
+							final float z = positionElement.getAsJsonArray().get(1).getAsFloat();
+							renderOnceFlipped(part, matrices, vertices, light, x, z - zOffset);
+						});
+					}
+				}
+			}
+		});
+	}
+
+	private static <T> void getArrayFromValue(T[] array, JsonObject jsonObject, String key, Function<JsonElement, T> consumer) {
+		if (jsonObject.has(key)) {
+			final JsonArray jsonArray = jsonObject.getAsJsonArray(key);
+			for (int i = 0; i < array.length; i++) {
+				array[i] = consumer.apply(jsonArray.get(i));
+			}
+		}
+	}
+}
