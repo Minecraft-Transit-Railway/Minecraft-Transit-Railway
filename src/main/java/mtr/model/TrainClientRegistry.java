ceb081143ada122e5563a9565d2394f1cc1ed5b1
Jonathan Ho <jonafanho@gmail.com>
2021-12-09 23:37:01 -0800

    London Underground 1995 sounds

--- src/main/java/mtr/model/TrainClientRegistry.java
+++ src/main/java/mtr/model/TrainClientRegistry.java
@@ -83,7 +83,7 @@ public class TrainClientRegistry {
 		register("light_rail_3_orange", TrainType.LIGHT_RAIL_3_ORANGE, new ModelLightRail(3), "mtr:textures/entity/light_rail_3_orange", "light_rail", "light_rail_3", null, 0xD2A825, false, 48, 1, false);
 		register("light_rail_4_orange", TrainType.LIGHT_RAIL_4_ORANGE, new ModelLightRail(4), "mtr:textures/entity/light_rail_4_orange", "light_rail", "light_rail_4", null, 0xD2A825, false, 48, 1, false);
 		register("light_rail_5_orange", TrainType.LIGHT_RAIL_5_ORANGE, new ModelLightRail(5), "mtr:textures/entity/light_rail_5_orange", "light_rail", "light_rail_4", null, 0xD2A825, false, 48, 1, false);
-		register("london_underground_1995", TrainType.LONDON_UNDERGROUND_1995, new ModelLondonUnderground1995(), "mtr:textures/entity/london_underground_1995", "r179", "london_underground_1995", null, 0x333333, false, 0, 0.5F, false);
+		register("london_underground_1995", TrainType.LONDON_UNDERGROUND_1995, new ModelLondonUnderground1995(), "mtr:textures/entity/london_underground_1995", "london_underground_1995", "london_underground_1995", null, 0x333333, false, 27, 0.5F, false);
 		register("r179", TrainType.R179, new ModelR179(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, false, 66, 1, false);
 		register("r179_mini", TrainType.R179_MINI, new ModelR179Mini(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, false, 66, 1, false);
 		register("minecart", TrainType.MINECART, null, null, null, null, null, 0x666666, false, 0, 0.5F, false);
