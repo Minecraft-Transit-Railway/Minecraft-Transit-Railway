5672c0e35b48a269f1292dbff1242f5df067f0a7
ExtremeMakerX <79700465+ExtremeMakerX@users.noreply.github.com>
2021-12-14 22:12:23 -0500

    Fixing Build Error 2

--- src/main/java/mtr/gui/SidingScreen.java
+++ src/main/java/mtr/gui/SidingScreen.java
@@ -161,7 +161,6 @@ public class SidingScreen extends SavedRailScreenBase<Siding> {
 		buttonTrainBarrier.visible = isSelectingTrain;
 		buttonSelectTrain.setMessage(TrainClientRegistry.getTrainProperties(savedRailBase.getTrainId(), savedRailBase.getBaseTrainType()).name);
 		availableTrainsList.x = isSelectingTrain ? width / 2 - PANEL_WIDTH / 2 : width;
-		addButton(buttonTrainBarrier);
 	}
 
 	private void onAdd(NameColorDataBase data, int index) {
