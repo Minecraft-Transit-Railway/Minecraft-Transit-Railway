043ce3c7f6be30fb9bf232551cf799f166971186
Eric28020 <55635216+Eric28020@users.noreply.github.com>
2022-01-13 03:09:37 -0800

    Preparing the London Underground 1996 Stock

--- common/src/main/java/mtr/client/TrainClientRegistry.java
+++ common/src/main/java/mtr/client/TrainClientRegistry.java
@@ -90,6 +90,7 @@ public class TrainClientRegistry {
 		register("light_rail_4_orange", TrainType.LIGHT_RAIL_4_ORANGE, new ModelLightRail(4), "mtr:textures/entity/light_rail_4_orange", "light_rail", "light_rail_4", null, 0xD2A825, false, 48, 1, false);
 		register("light_rail_5_orange", TrainType.LIGHT_RAIL_5_ORANGE, new ModelLightRail(5), "mtr:textures/entity/light_rail_5_orange", "light_rail", "light_rail_4", null, 0xD2A825, false, 48, 1, false);
 		register("london_underground_1995", TrainType.LONDON_UNDERGROUND_1995, new ModelLondonUnderground1995(), "mtr:textures/entity/london_underground_1995", "london_underground_1995", "london_underground_1995", null, 0x333333, false, 27, 0.5F, false);
+		register("london_underground_1996", TrainType.LONDON_UNDERGROUND_1996, new ModelLondonUnderground1995(), "mtr:textures/entity/london_underground_1996", "london_underground_1995", "london_underground_1995", null, 0x868F98, false, 27, 0.5F, false);
 		register("r179", TrainType.R179, new ModelR179(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, false, 66, 1, false);
 		register("r179_mini", TrainType.R179_MINI, new ModelR179Mini(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, false, 66, 1, false);
 		register("minecart", TrainType.MINECART, null, "textures/entity/minecart", null, null, null, 0x666666, false, 0, 0.5F, false);
