b639097d115799ce4617e6d9b864b2721ba115ec
Nemo <zbx1425@outlook.com>
2022-01-26 11:08:59 +0800

    Fix destination not shown at same line transfer

--- common/src/main/resources/assets/mtr/website/tripplan.js
+++ common/src/main/resources/assets/mtr/website/tripplan.js
@@ -19,7 +19,8 @@ function buildGraph(data) {
                         let stationId = route["stations"][parseInt(si2)+1].split("_")[0];
                         adjTable[keyName][stationId] = { "route": routeIndex };
                     }
-                    break;
+                    // Should not break, a route might contain the same station multiple times
+                    // break;
                 }
             }
         }
@@ -27,6 +28,7 @@ function buildGraph(data) {
 }
 
 function findRoute(data, beginId, endId) {
+    // BFS
     let vis = {};
     let fa = {};
     let queue = [ beginId ];
@@ -93,31 +95,35 @@ function findShowPlan(data, planner_begin, planner_end) {
     let plan = findRoute(data, planner_begin, planner_end);
     if (plan == null) {
         if (planner_begin === 0 || planner_end === 0) {
+            // Hint clicking the two buttons
             html = "<span class='material-icons'>info</span>: <span class='material-icons'>mouse</span> <span class='material-icons'>arrow_right_alt</span>" +
                 "\"<span class='material-icons'>home</span>, <span class='material-icons'>flag</span>\"";
         } else {
+            // No route
             html = "<span class='material-icons'>warning</span>: <span class='material-icons'>route</span> <span class='material-icons'>clear</span>"
         }
+    } else if (plan.length == 0) {
+        // Recommend walking
+        html = "<span class='material-icons'>info</span>: <span class='material-icons'>directions_walk</span>"
     } else {
         let lastStep = { "to": planner_begin, "route": 0 };
         html = "";
         for (const stepId in plan) {
             const step = plan[stepId];
             if (step["route"] != lastStep["route"]) {
-                if (data["routes"][lastStep["route"]]["name"] === data["routes"][step["route"]]["name"]
-                    && data["routes"][lastStep["route"]]["color"] === data["routes"][step["route"]]["color"]) {
-                    lastStep = step;
-                    continue;
+                // Do not show the transfer between rapid trains and local trains of the same line
+                if (lastStep["route"] === 0 || data["routes"][lastStep["route"]]["name"] !== data["routes"][step["route"]]["name"]
+                    || data["routes"][lastStep["route"]]["color"] !== data["routes"][step["route"]]["color"]) {
+                    if (lastStep["route"] === 0) {
+                        html += "<div><span class='material-icons'>home</span>" + smallerEnglish(data["stations"][lastStep["to"]]["name"]) + "</div>";
+                    } else {
+                        html += "<div><span class='material-icons'>transfer_within_a_station</span>" + smallerEnglish(data["stations"][lastStep["to"]]["name"]) + "</div>";
+                    }
+                    html += "<div style='background-color: " + convertColor(data["routes"][step["route"]]["color"]) + "; " 
+                        + "color: " + hex_inverse_bw(convertColor(data["routes"][step["route"]]["color"])) + "; font-weight: bold'>" 
+                        + "<span class='material-icons' style='color:inherit'>tram</span>"
+                        + smallerEnglish(data["routes"][step["route"]]["name"]) + "</div>";
                 }
-                if (lastStep["route"] === 0) {
-                    html += "<div><span class='material-icons'>home</span>" + smallerEnglish(data["stations"][lastStep["to"]]["name"]) + "</div>";
-                } else {
-                    html += "<div><span class='material-icons'>transfer_within_a_station</span>" + smallerEnglish(data["stations"][lastStep["to"]]["name"]) + "</div>";
-                }
-                html += "<div style='background-color: " + convertColor(data["routes"][step["route"]]["color"]) + "; " 
-                    + "color: " + hex_inverse_bw(convertColor(data["routes"][step["route"]]["color"])) + "; font-weight: bold'>" 
-                    + "<span class='material-icons' style='color:inherit'>tram</span>"
-                    + smallerEnglish(data["routes"][step["route"]]["name"]) + "</div>";
             }
             if (step["to"] === planner_end) {
                 html += "<div><span class='material-icons'>flag</span>" + smallerEnglish(data["stations"][step["to"]]["name"]) + "</div>";
