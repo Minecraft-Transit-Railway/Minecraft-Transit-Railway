25ae966c91baefa52a340495a3e74037a143c026
Marcus Kwok <marcuskwok2004@gmail.com>
2022-02-09 13:21:45 +0800

    Mini and Small variants
    Well it looks kinda cursed, but whatever

---
+++ common/src/main/java/mtr/model/ModelSTrainMini.java
@@ -0,0 +1,19 @@
+package mtr.model;
+
+public class ModelSTrainMini extends ModelMTrain {
+
+	@Override
+	protected int[] getWindowPositions() {
+		return new int[]{0};
+	}
+
+	@Override
+	protected int[] getDoorPositions() {
+		return new int[]{-40, 40};
+	}
+
+	@Override
+	protected int[] getEndPositions() {
+		return new int[]{-64, 64};
+	}
+}
