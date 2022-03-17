3a947248bf52158778f1a0df36a99c43c058b590
Jonathan Ho <jonafanho@gmail.com>
2022-03-17 16:15:08 -0700

    R211 model review

--- common/src/main/java/mtr/client/TrainClientRegistry.java
+++ common/src/main/java/mtr/client/TrainClientRegistry.java
@@ -101,10 +101,10 @@ public class TrainClientRegistry {
 		register("london_underground_1996", TrainType.LONDON_UNDERGROUND_1996, new ModelLondonUnderground1995(), "mtr:textures/entity/london_underground_1996", "london_underground_1995", "london_underground_1995", null, 0xA1A5A7, 27, 0.5F, false);
 		register("r179", TrainType.R179, new ModelR179(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
 		register("r179_mini", TrainType.R179_MINI, new ModelR179Mini(), "mtr:textures/entity/r179", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
-		register("r211", TrainType.R211, new ModelR211(false), "mtr:textures/entity/r211", null, "r179", null, 0xD5D5D5, 0, 1, false);
-		register("r211_mini", TrainType.R211_MINI, new ModelR211Mini(false), "mtr:textures/entity/r211", null, "r179", null, 0xD5D5D5, 0, 1, false);
-		register("r211t", TrainType.R211T, new ModelR211(true), "mtr:textures/entity/r211", null, "r179", null, 0xD5D5D5, 0, 1, false);
-		register("r211t_mini", TrainType.R211T_MINI, new ModelR211Mini(true), "mtr:textures/entity/r211", null, "r179", null, 0xD5D5D5, 0, 1, false);
+		register("r211", TrainType.R211, new ModelR211(false), "mtr:textures/entity/r211", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
+		register("r211_mini", TrainType.R211_MINI, new ModelR211Mini(false), "mtr:textures/entity/r211", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
+		register("r211t", TrainType.R211T, new ModelR211(true), "mtr:textures/entity/r211", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
+		register("r211t_mini", TrainType.R211T_MINI, new ModelR211Mini(true), "mtr:textures/entity/r211", "r179", "r179", null, 0xD5D5D5, 66, 1, false);
 		register("minecart", TrainType.MINECART, null, "textures/entity/minecart", null, null, null, 0x666666, 0, 0.5F, false);
 		register("oak_boat", TrainType.OAK_BOAT, null, "textures/entity/boat/oak", null, null, null, 0x8F7748, 0, 0.5F, false);
 		register("spruce_boat", TrainType.SPRUCE_BOAT, null, "textures/entity/boat/spruce", null, null, null, 0x8F7748, 0, 0.5F, false);
