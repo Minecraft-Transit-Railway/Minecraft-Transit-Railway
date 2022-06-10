a2a721ccfb61da657e7b24b000dff608059e5b8d
LX862 <kenny.mh.hui@gmail.com>
2022-06-10 12:57:19 +0800

    Increase dwell time

--- common/src/main/java/mtr/screen/PlatformScreen.java
+++ common/src/main/java/mtr/screen/PlatformScreen.java
@@ -9,24 +9,33 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 public class PlatformScreen extends SavedRailScreenBase<Platform> {
-
 	private static final Component DWELL_TIME_TEXT = Text.translatable("gui.mtr.dwell_time");
-	private final WidgetShorterSlider sliderDwellTime;
+	private static final int MAX_SEC_VALUE = 119;
+	private final WidgetShorterSlider sliderDwellTimeSec;
+	private final WidgetShorterSlider sliderDwellTimeMin;
 
 	public PlatformScreen(Platform savedRailBase, TransportMode transportMode, DashboardScreen dashboardScreen) {
 		super(savedRailBase, transportMode, dashboardScreen, DWELL_TIME_TEXT);
-		sliderDwellTime = new WidgetShorterSlider(0, SLIDER_WIDTH - font.width("88.8s") - TEXT_PADDING, Platform.MAX_DWELL_TIME - 1, value -> String.format("%ss", (value + 1) / 2F), null);
+		sliderDwellTimeMin = new WidgetShorterSlider(0, SLIDER_WIDTH - font.width("88min") - TEXT_PADDING, (int) Math.floor((Platform.MAX_DWELL_TIME / 2.0) / 60.0), value -> String.format("%smin", (value)), null);
+		sliderDwellTimeSec = new WidgetShorterSlider(0, SLIDER_WIDTH - font.width("88.8s") - TEXT_PADDING, MAX_SEC_VALUE, 10, 2, value -> String.format("%ss", (value) / 2F), null);
 	}
 
 	@Override
 	protected void init() {
 		super.init();
-		sliderDwellTime.x = startX + textWidth;
-		sliderDwellTime.y = height / 2 + TEXT_FIELD_PADDING / 2;
-		sliderDwellTime.setHeight(SQUARE_SIZE);
-		sliderDwellTime.setValue(savedRailBase.getDwellTime() - 1);
+		sliderDwellTimeMin.x = startX + textWidth;
+		sliderDwellTimeMin.y = height / 2 + TEXT_FIELD_PADDING / 2;
+		sliderDwellTimeMin.setHeight(SQUARE_SIZE);
+		sliderDwellTimeMin.setValue((int) Math.floor((savedRailBase.getDwellTime() / 2.0) / 60.0));
+
+		sliderDwellTimeSec.x = startX + textWidth;
+		sliderDwellTimeSec.y = (height / 2 + TEXT_FIELD_PADDING / 2) + sliderDwellTimeMin.getHeight();
+		sliderDwellTimeSec.setHeight(SQUARE_SIZE);
+		sliderDwellTimeSec.setValue(savedRailBase.getDwellTime() % 120);
+
 		if (showScheduleControls) {
-			addDrawableChild(sliderDwellTime);
+			addDrawableChild(sliderDwellTimeMin);
+			addDrawableChild(sliderDwellTimeSec);
 		}
 	}
 
@@ -39,8 +48,22 @@ public class PlatformScreen extends SavedRailScreenBase<Platform> {
 	}
 
 	@Override
+	public void tick() {
+		int maxMin = (int) Math.floor(Platform.MAX_DWELL_TIME / 2.0) / 60;
+		if(sliderDwellTimeMin.getIntValue() == 0 && sliderDwellTimeSec.getIntValue() == 0) {
+			sliderDwellTimeSec.setValue(1);
+		}
+
+		if(sliderDwellTimeMin.getIntValue() == maxMin && sliderDwellTimeSec.getIntValue() > Platform.MAX_DWELL_TIME % 120) {
+			sliderDwellTimeSec.setValue(Platform.MAX_DWELL_TIME % 120);
+		}
+	}
+
+	@Override
 	public void onClose() {
-		savedRailBase.setDwellTime(sliderDwellTime.getIntValue() + 1, packet -> PacketTrainDataGuiClient.sendUpdate(PACKET_UPDATE_PLATFORM, packet));
+		int minutes = sliderDwellTimeMin.getIntValue();
+		double second = sliderDwellTimeSec.getIntValue() / 2.0;
+		savedRailBase.setDwellTime((int)((second + minutes * 60) * 2), packet -> PacketTrainDataGuiClient.sendUpdate(PACKET_UPDATE_PLATFORM, packet));
 		super.onClose();
 	}
 
