cb1691b50f69c6e022c551c6f6a0156db8536323
LX862 <kenny.mh.hui@gmail.com>
2022-08-12 02:55:07 +0800

    Lockable doors

--- common/src/main/java/mtr/block/BlockPSDAPGDoorBase.java
+++ common/src/main/java/mtr/block/BlockPSDAPGDoorBase.java
@@ -1,8 +1,11 @@
 package mtr.block;
 
+import mtr.mappings.Text;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -14,6 +17,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -24,6 +28,7 @@ public abstract class BlockPSDAPGDoorBase extends BlockPSDAPGBase {
 
 	public static final BooleanProperty END = BooleanProperty.create("end");
 	public static final BooleanProperty ODD = BooleanProperty.create("odd");
+	public static final BooleanProperty UNLOCKED = BooleanProperty.create("unlocked");
 	public static final IntegerProperty OPEN = IntegerProperty.create("open", 0, MAX_OPEN_VALUE);
 
 	@Override
@@ -60,12 +65,46 @@ public abstract class BlockPSDAPGDoorBase extends BlockPSDAPGBase {
 	}
 
 	@Override
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+		return IBlock.checkHoldingBrush(world, player, () -> {
+			boolean unlocked = IBlock.getStatePropertySafe(state, UNLOCKED);
+			for (int y = -1; y <= 1; y++) {
+				final BlockState scanState = world.getBlockState(pos.above(y));
+				if (state.is(scanState.getBlock())) {
+					lockDoor(world, pos.above(y), scanState, !unlocked);
+				}
+			}
+			player.displayClientMessage(!unlocked ? Text.translatable("gui.mtr.psdapg_door_unlocked") : Text.translatable("gui.mtr.psdapg_door_locked"), true);
+		});
+	}
+
+	public void lockDoor(Level world, BlockPos pos, BlockState state, boolean unlocked) {
+		final Direction facing = IBlock.getStatePropertySafe(state, FACING);
+		final BlockPos leftPos = pos.relative(facing.getCounterClockWise());
+		final BlockPos rightPos = pos.relative(facing.getClockWise());
+		final BlockState leftState = world.getBlockState(leftPos);
+		final BlockState rightState = world.getBlockState(rightPos);
+
+		if(leftState.is(state.getBlock())) {
+			final BlockState toggled = leftState.setValue(UNLOCKED, unlocked);
+			world.setBlockAndUpdate(leftPos, toggled);
+		}
+
+		if(rightState.is(state.getBlock())) {
+			final BlockState toggled = rightState.setValue(UNLOCKED, unlocked);
+			world.setBlockAndUpdate(rightPos, toggled);
+		}
+
+		world.setBlockAndUpdate(pos, state.setValue(UNLOCKED, unlocked));
+	}
+
+	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext collisionContext) {
 		return IBlock.getStatePropertySafe(state, OPEN) > 0 ? Shapes.empty() : super.getCollisionShape(state, world, pos, collisionContext);
 	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(END, FACING, HALF, OPEN, SIDE);
+		builder.add(END, FACING, HALF, OPEN, SIDE, UNLOCKED);
 	}
 }
