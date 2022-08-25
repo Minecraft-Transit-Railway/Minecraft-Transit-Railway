7ea3b6a2422cddf15df6d1022937d99c177ac466
BDark000 <95154563+BDark000@users.noreply.github.com>
2022-08-25 14:15:00 +0800

    Always display custom text messages

--- common/src/main/java/mtr/client/IDrawing.java
+++ common/src/main/java/mtr/client/IDrawing.java
@@ -186,11 +186,9 @@ public interface IDrawing {
 			if (Config.useTTSAnnouncements()) {
 				Narrator.getNarrator().say(newMessage, true);
 			}
-			if (Config.genAnnouncementMessages()) {
-				final Player player = Minecraft.getInstance().player;
-				if (player != null) {
-					player.displayClientMessage(Text.literal(newMessage), false);
-				}
+			final Player player = Minecraft.getInstance().player;
+			if (player != null) {
+				player.displayClientMessage(Text.literal(newMessage), false);
 			}
 		}
 	}
