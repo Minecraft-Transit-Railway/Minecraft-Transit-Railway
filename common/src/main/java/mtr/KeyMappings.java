13ef7fce6be21e7eae3b9cefbb49dc9357625818
yo56789 <71994752+yo56789@users.noreply.github.com>
2022-08-25 15:59:08 -0400

    Change KeyMapping for custom rail menu

--- common/src/main/java/mtr/KeyMappings.java
+++ common/src/main/java/mtr/KeyMappings.java
@@ -10,5 +10,5 @@ public interface KeyMappings {
 	KeyMapping TRAIN_ACCELERATE = new KeyMapping("key.mtr.train_accelerate", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UP, "category.mtr.keybinding");
 	KeyMapping TRAIN_BRAKE = new KeyMapping("key.mtr.train_brake", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_DOWN, "category.mtr.keybinding");
 	KeyMapping TRAIN_TOGGLE_DOORS = new KeyMapping("key.mtr.train_toggle_doors", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT, "category.mtr.keybinding");
-	KeyMapping CUSTOM_RAIL_MENU = new KeyMapping("key.mtr.custom_rail_menu", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_M, "category.mtr.keybinding");
+	KeyMapping CUSTOM_RAIL_MENU = new KeyMapping("key.mtr.custom_rail_menu", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, "category.mtr.keybinding");
 }
