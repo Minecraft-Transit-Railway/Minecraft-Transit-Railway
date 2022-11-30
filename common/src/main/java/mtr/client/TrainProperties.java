d112e97f6c591d452fa72c64bc6f62f56db5867e
Jonathan Ho <jonafanho@gmail.com>
2022-11-29 21:28:32 -0800

    Fixed some merge conflicts and bump version

--- common/src/main/java/mtr/client/TrainProperties.java
+++ common/src/main/java/mtr/client/TrainProperties.java
@@ -9,35 +9,37 @@ import net.minecraft.network.chat.Component;
 
 public class TrainProperties {
 
-    public final String baseTrainType;
-    public final Component name;
-    public final Component description;
-    public final int color;
-    public final float riderOffset;
-    public final float bogiePosition;
-    public final boolean isJacobsBogie;
-    public final boolean hasGangwayConnection;
-    public final TrainRendererBase renderer;
-    public final TrainSoundBase sound;
+	public final String baseTrainType;
+	public final Component name;
+	public final Component description;
+	public final int color;
+	public final float riderOffset;
+	public final float riderOffsetDismounting;
+	public final float bogiePosition;
+	public final boolean isJacobsBogie;
+	public final boolean hasGangwayConnection;
+	public final TrainRendererBase renderer;
+	public final TrainSoundBase sound;
 
-    public TrainProperties(String baseTrainType, Component name, Component description, int color, float riderOffset, float bogiePosition, boolean isJacobsBogie, boolean hasGangwayConnection, TrainRendererBase renderer, TrainSoundBase sound) {
-        this.baseTrainType = baseTrainType;
-        this.name = name;
-        this.description = description;
-        this.color = color;
-        this.riderOffset = riderOffset;
-        this.bogiePosition = bogiePosition;
-        this.isJacobsBogie = isJacobsBogie;
-        this.hasGangwayConnection = hasGangwayConnection;
-        this.renderer = renderer;
-        this.sound = sound;
-    }
+	public TrainProperties(String baseTrainType, Component name, Component description, int color, float riderOffset, float riderOffsetDismounting, float bogiePosition, boolean isJacobsBogie, boolean hasGangwayConnection, TrainRendererBase renderer, TrainSoundBase sound) {
+		this.baseTrainType = baseTrainType;
+		this.name = name;
+		this.description = description;
+		this.color = color;
+		this.riderOffset = riderOffset;
+		this.riderOffsetDismounting = riderOffsetDismounting;
+		this.bogiePosition = bogiePosition;
+		this.isJacobsBogie = isJacobsBogie;
+		this.hasGangwayConnection = hasGangwayConnection;
+		this.renderer = renderer;
+		this.sound = sound;
+	}
 
-    public static TrainProperties getBlankProperties() {
-        return new TrainProperties(
-                "", Text.translatable(""), null, 0, 0, 0, false, false,
-                new JonModelTrainRenderer(null, "", "", ""),
-                new JonTrainSound("", new JonTrainSound.JonTrainSoundConfig(null, 0, 0.5F, false))
-        );
-    }
+	public static TrainProperties getBlankProperties() {
+		return new TrainProperties(
+				"", Text.translatable(""), null, 0, 0, 0, 0, false, false,
+				new JonModelTrainRenderer(null, "", "", ""),
+				new JonTrainSound("", new JonTrainSound.JonTrainSoundConfig(null, 0, 0.5F, false))
+		);
+	}
 }
