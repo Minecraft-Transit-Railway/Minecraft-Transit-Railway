0498aea3472efcc28db3250266206a59e5212476
Jonathan Ho <jonafanho@gmail.com>
2023-11-15 00:05:05 -0800

    Converted more endpoints to JSON generated objects

--- fabric/src/main/java/org/mtr/mod/packet/PacketData.java
+++ fabric/src/main/java/org/mtr/mod/packet/PacketData.java
@@ -8,12 +8,13 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.mtr.core.data.Position;
 import org.mtr.core.data.*;
+import org.mtr.core.integration.Integration;
+import org.mtr.core.integration.Response;
 import org.mtr.core.serializer.JsonReader;
 import org.mtr.core.serializer.SerializedDataBase;
 import org.mtr.core.servlet.IntegrationServlet;
 import org.mtr.core.tool.EnumHelper;
 import org.mtr.core.tool.Utilities;
-import org.mtr.libraries.com.google.gson.JsonArray;
 import org.mtr.libraries.com.google.gson.JsonObject;
 import org.mtr.libraries.com.google.gson.JsonParser;
 import org.mtr.libraries.it.unimi.dsi.fastutil.longs.LongAVLTreeSet;
@@ -37,7 +38,7 @@ import java.util.function.Function;
 public class PacketData extends PacketHandler {
 
 	protected final IntegrationServlet.Operation operation;
-	private final JsonObject jsonObject;
+	private final Integration integration;
 
 	public static PacketData fromStations(IntegrationServlet.Operation operation, ObjectSet<Station> dataSet) {
 		return new PacketData(operation, dataSet, null, null, null, null, null, null, null);
@@ -71,32 +72,37 @@ public class PacketData extends PacketHandler {
 		return new PacketData(IntegrationServlet.Operation.UPDATE, null, null, null, null, null, null, null, ObjectSet.of(signalModification));
 	}
 
-	private PacketData(IntegrationServlet.Operation operation, ObjectSet<Station> stations, ObjectSet<Platform> platforms, ObjectSet<Siding> sidings, ObjectSet<Route> routes, ObjectSet<Depot> depots, ObjectSet<Rail> rails, ObjectSet<Position> positions, ObjectSet<SignalModification> signalModifications) {
+	private PacketData(
+			IntegrationServlet.Operation operation,
+			@Nullable ObjectSet<Station> stations,
+			@Nullable ObjectSet<Platform> platforms,
+			@Nullable ObjectSet<Siding> sidings,
+			@Nullable ObjectSet<Route> routes,
+			@Nullable ObjectSet<Depot> depots,
+			@Nullable ObjectSet<Rail> rails,
+			@Nullable ObjectSet<Position> positions,
+			@Nullable ObjectSet<SignalModification> signalModifications
+	) {
 		this.operation = operation;
-		jsonObject = new JsonObject();
-		jsonObject.add("stations", writeDataSetToJsonArray(stations));
-		jsonObject.add("platforms", writeDataSetToJsonArray(platforms));
-		jsonObject.add("sidings", writeDataSetToJsonArray(sidings));
-		jsonObject.add("routes", writeDataSetToJsonArray(routes));
-		jsonObject.add("depots", writeDataSetToJsonArray(depots));
-		jsonObject.add("rails", writeDataSetToJsonArray(rails));
-		jsonObject.add("positions", writeDataSetToJsonArray(positions));
-		jsonObject.add("signals", writeDataSetToJsonArray(signalModifications));
+		integration = new Integration();
+		integration.add(stations, platforms, sidings, routes, depots);
+		integration.add(rails, positions);
+		integration.add(signalModifications);
 	}
 
-	public PacketData(IntegrationServlet.Operation operation, JsonObject jsonObject) {
+	public PacketData(IntegrationServlet.Operation operation, Integration integration) {
 		this.operation = operation;
-		this.jsonObject = jsonObject;
+		this.integration = integration;
 	}
 
 	public PacketData(PacketBuffer packetBuffer) {
-		this(EnumHelper.valueOf(IntegrationServlet.Operation.UPDATE, readString(packetBuffer)), parseJson(readString(packetBuffer)));
+		this(EnumHelper.valueOf(IntegrationServlet.Operation.UPDATE, readString(packetBuffer)), new Integration(new JsonReader(parseJson(readString(packetBuffer)))));
 	}
 
 	@Override
 	public void write(PacketBuffer packetBuffer) {
 		writeString(packetBuffer, operation.toString());
-		writeString(packetBuffer, jsonObject.toString());
+		writeString(packetBuffer, Utilities.getJsonObjectFromData(integration).toString());
 	}
 
 	@Override
@@ -108,62 +114,55 @@ public class PacketData extends PacketHandler {
 	public void runClientQueued() {
 		final Screen screen = MinecraftClient.getInstance().getCurrentScreenMapped();
 		if (operation != IntegrationServlet.Operation.LIST || screen == null || !(screen.data instanceof ScreenExtension)) {
-			writeJsonObjectToDataSet(ClientData.instance.stations, jsonObject.getAsJsonArray("stations"), jsonReader -> new Station(jsonReader, ClientData.instance), NameColorDataBase::getId);
-			writeJsonObjectToDataSet(ClientData.instance.platforms, jsonObject.getAsJsonArray("platforms"), jsonReader -> new Platform(jsonReader, ClientData.instance), NameColorDataBase::getId);
-			writeJsonObjectToDataSet(ClientData.instance.sidings, jsonObject.getAsJsonArray("sidings"), jsonReader -> new Siding(jsonReader, ClientData.instance), NameColorDataBase::getId);
-			writeJsonObjectToDataSet(ClientData.instance.routes, jsonObject.getAsJsonArray("routes"), jsonReader -> new Route(jsonReader, ClientData.instance), NameColorDataBase::getId);
-			writeJsonObjectToDataSet(ClientData.instance.depots, jsonObject.getAsJsonArray("depots"), jsonReader -> new Depot(jsonReader, ClientData.instance), NameColorDataBase::getId);
-			writeJsonObjectToDataSet(ClientData.instance.rails, jsonObject.getAsJsonArray("rails"), Rail::new, Rail::getHexId);
-			writeJsonObjectToDataSet(jsonObject.getAsJsonArray("update"), jsonObject.getAsJsonArray("keep"));
+			if (integration.hasData()) {
+				writeJsonObjectToDataSet(ClientData.instance.stations, integration::iterateStations, NameColorDataBase::getId);
+				writeJsonObjectToDataSet(ClientData.instance.platforms, integration::iteratePlatforms, NameColorDataBase::getId);
+				writeJsonObjectToDataSet(ClientData.instance.sidings, integration::iterateSidings, NameColorDataBase::getId);
+				writeJsonObjectToDataSet(ClientData.instance.routes, integration::iterateRoutes, NameColorDataBase::getId);
+				writeJsonObjectToDataSet(ClientData.instance.depots, integration::iterateDepots, NameColorDataBase::getId);
+				writeJsonObjectToDataSet(ClientData.instance.rails, integration::iterateRails, Rail::getHexId);
+			}
+
+			if (integration.hasVehicle()) {
+				final LongAVLTreeSet keepVehicleIds = new LongAVLTreeSet();
+				integration.iterateVehiclesToKeep(keepVehicleIds::add);
+				ClientData.instance.vehicles.removeIf(vehicle -> !keepVehicleIds.contains(vehicle.getId()));
+				integration.iterateVehiclesToUpdate(vehicleUpdate -> ClientData.instance.vehicles.add(new VehicleExtension(vehicleUpdate, ClientData.instance)));
+			}
+
 			ClientData.instance.vehicles.forEach(vehicle -> vehicle.vehicleExtraData.immutablePath.forEach(pathData -> pathData.writePathCache(ClientData.instance)));
 			ClientData.instance.sync();
 		}
 	}
 
 	private void sendHttpRequestAndBroadcastResultToAllPlayers(ServerWorld serverWorld) {
-		sendHttpDataRequest(operation, jsonObject, data -> {
+		sendHttpDataRequest(operation, integration, newIntegration -> {
 			// Check if there are any rail nodes that need to be reset
-			final JsonArray positionsArray = data.getAsJsonArray("positions");
-			if (positionsArray != null) {
-				positionsArray.forEach(jsonElement -> BlockNode.resetRailNode(serverWorld, Init.positionToBlockPos(new Position(new JsonReader(jsonElement)))));
-			}
-
+			integration.iteratePositions(position -> BlockNode.resetRailNode(serverWorld, Init.positionToBlockPos(position)));
 			// Broadcast result to all players
-			MinecraftServerHelper.iteratePlayers(serverWorld, worldPlayer -> Registry.sendPacketToClient(worldPlayer, new PacketData(operation, data)));
+			MinecraftServerHelper.iteratePlayers(serverWorld, worldPlayer -> Registry.sendPacketToClient(worldPlayer, new PacketData(operation, newIntegration)));
 		});
 	}
 
-	private <T extends SerializedDataBase, U> void writeJsonObjectToDataSet(ObjectSet<T> dataSet, @Nullable JsonArray jsonArray, Function<JsonReader, T> createInstance, Function<T, U> getId) {
-		if (jsonArray != null) {
-			final ObjectArraySet<T> newData = new ObjectArraySet<>();
-			final ObjectOpenHashSet<U> idList = new ObjectOpenHashSet<>();
+	private <T extends SerializedDataBase, U> void writeJsonObjectToDataSet(ObjectSet<T> dataSet, Consumer<Consumer<T>> iterator, Function<T, U> getId) {
+		final ObjectArraySet<T> newData = new ObjectArraySet<>();
+		final ObjectOpenHashSet<U> idList = new ObjectOpenHashSet<>();
 
-			jsonArray.forEach(jsonElement -> {
-				final T data = createInstance.apply(new JsonReader(jsonElement.getAsJsonObject()));
-				newData.add(data);
-				idList.add(getId.apply(data));
-			});
+		iterator.accept(data -> {
+			newData.add(data);
+			idList.add(getId.apply(data));
+		});
 
-			if (operation == IntegrationServlet.Operation.LIST) {
-				dataSet.clear();
-			}
-
-			if ((operation == IntegrationServlet.Operation.UPDATE || operation == IntegrationServlet.Operation.DELETE) && !idList.isEmpty()) {
-				dataSet.removeIf(data -> idList.contains(getId.apply(data)));
-			}
-
-			if (operation == IntegrationServlet.Operation.UPDATE || operation == IntegrationServlet.Operation.LIST) {
-				dataSet.addAll(newData);
-			}
+		if (operation == IntegrationServlet.Operation.LIST) {
+			dataSet.clear();
 		}
-	}
 
-	private void writeJsonObjectToDataSet(@Nullable JsonArray vehicleUpdateArray, @Nullable JsonArray keepVehicleIdsArray) {
-		if (vehicleUpdateArray != null && keepVehicleIdsArray != null) {
-			final LongAVLTreeSet keepVehicleIds = new LongAVLTreeSet();
-			keepVehicleIdsArray.forEach(vehicleIdElement -> keepVehicleIds.add(vehicleIdElement.getAsLong()));
-			ClientData.instance.vehicles.removeIf(vehicle -> !keepVehicleIds.contains(vehicle.getId()));
-			vehicleUpdateArray.forEach(vehicleElement -> ClientData.instance.vehicles.add(new VehicleExtension(vehicleElement.getAsJsonObject(), ClientData.instance)));
+		if ((operation == IntegrationServlet.Operation.UPDATE || operation == IntegrationServlet.Operation.DELETE) && !idList.isEmpty()) {
+			dataSet.removeIf(data -> idList.contains(getId.apply(data)));
+		}
+
+		if (operation == IntegrationServlet.Operation.UPDATE || operation == IntegrationServlet.Operation.LIST) {
+			dataSet.addAll(newData);
 		}
 	}
 
@@ -184,7 +183,7 @@ public class PacketData extends PacketHandler {
 	}
 
 	public static void sendHttpRequest(String endpoint, JsonObject contentObject, Consumer<JsonObject> consumer) {
-		final HttpPost request = new HttpPost("http://localhost:8888/" + endpoint);
+		final HttpPost request = new HttpPost("http://localhost:8888/mtr/api/" + endpoint);
 		request.addHeader("content-type", "application/json");
 
 		try {
@@ -209,15 +208,7 @@ public class PacketData extends PacketHandler {
 		}
 	}
 
-	protected static void sendHttpDataRequest(IntegrationServlet.Operation operation, JsonObject contentObject, Consumer<JsonObject> consumer) {
-		sendHttpRequest("mtr/api/data/" + operation.getEndpoint(), contentObject, data -> consumer.accept(data.getAsJsonObject("data")));
-	}
-
-	private static <T extends SerializedDataBase> JsonArray writeDataSetToJsonArray(ObjectSet<T> dataSet) {
-		final JsonArray jsonArray = new JsonArray();
-		if (dataSet != null) {
-			dataSet.forEach(data -> jsonArray.add(Utilities.getJsonObjectFromData(data)));
-		}
-		return jsonArray;
+	protected static void sendHttpDataRequest(IntegrationServlet.Operation operation, Integration integration, Consumer<Integration> consumer) {
+		sendHttpRequest("data/" + operation.getEndpoint(), Utilities.getJsonObjectFromData(integration), data -> consumer.accept(Response.create(data).getData(Integration::new)));
 	}
 }
