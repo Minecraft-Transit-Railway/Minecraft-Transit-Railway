18f3de945ebe2dd7c9e0ef1b29bc1b7ead45d28e
YL Yang <XPGD_LHMTR@outlook.com>
2023-12-01 18:25:55 +0800

    Update InitClient.java
    Create 250km/h Ice Rail
    320km/h Nether Rail

--- fabric/src/main/java/org/mtr/mod/InitClient.java
+++ fabric/src/main/java/org/mtr/mod/InitClient.java
@@ -163,8 +163,12 @@ public final class InitClient {
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_160_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_200, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_200_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_250, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_250_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_300, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_300_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_320, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_320_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_PLATFORM, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_SIDING, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_TURN_BACK, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
