d85a3a919578c460e59b988b76337470249b8b04
YL Yang <XPGD_LHMTR@outlook.com>
2023-12-03 09:50:26 +0800

    Update InitClient.java

--- fabric/src/main/java/org/mtr/mod/InitClient.java
+++ fabric/src/main/java/org/mtr/mod/InitClient.java
@@ -155,10 +155,10 @@ public final class InitClient {
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_40_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_60, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_60_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
-		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_80, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
-		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_80_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_70, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_70_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_80, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
+		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_80_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_120, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_120_ONE_WAY, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
 		RegistryClient.registerItemModelPredicate(Items.RAIL_CONNECTOR_150, new Identifier(Init.MOD_ID, "selected"), checkItemPredicateTag());
