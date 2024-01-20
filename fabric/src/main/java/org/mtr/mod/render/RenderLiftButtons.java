adb6d40a7f69a89e4f121627f95bb8e977391436
Jonathan Ho <jonafanho@gmail.com>
2024-01-19 17:45:45 -0800

    Added lift floor display

--- fabric/src/main/java/org/mtr/mod/render/RenderLiftButtons.java
+++ fabric/src/main/java/org/mtr/mod/render/RenderLiftButtons.java
@@ -1,7 +1,10 @@
 package org.mtr.mod.render;
 
+import org.mtr.core.data.Lift;
 import org.mtr.core.data.LiftDirection;
+import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
 import org.mtr.mapping.holder.*;
 import org.mtr.mapping.mapper.BlockEntityRenderer;
 import org.mtr.mapping.mapper.DirectionHelper;
@@ -14,6 +17,8 @@ import org.mtr.mod.client.IDrawing;
 import org.mtr.mod.data.IGui;
 import org.mtr.mod.item.ItemLiftButtonsLinkModifier;
 
+import java.util.Comparator;
+
 public class RenderLiftButtons extends BlockEntityRenderer<BlockLiftButtons.BlockEntity> implements DirectionHelper, IGui, IBlock {
 
 	private static final int HOVER_COLOR = 0xFFFFAAAA;
@@ -25,7 +30,7 @@ public class RenderLiftButtons extends BlockEntityRenderer<BlockLiftButtons.Bloc
 	}
 
 	@Override
-	public void render(BlockLiftButtons.BlockEntity blockEntity, float tickDelta, GraphicsHolder graphicsHolder, int light, int overlay) {
+	public void render(BlockLiftButtons.BlockEntity blockEntity, float tickDelta, GraphicsHolder graphicsHolder1, int light, int overlay) {
 		final World world = blockEntity.getWorld2();
 		if (world == null) {
 			return;
@@ -41,15 +46,17 @@ public class RenderLiftButtons extends BlockEntityRenderer<BlockLiftButtons.Bloc
 		final Direction facing = IBlock.getStatePropertySafe(blockState, FACING);
 		final boolean holdingLinker = PlayerHelper.isHolding(PlayerEntity.cast(clientPlayerEntity), item -> item.data instanceof ItemLiftButtonsLinkModifier || Block.getBlockFromItem(item).data instanceof BlockLiftButtons);
 
-		graphicsHolder.push();
-		graphicsHolder.translate(0.5, 0, 0.5);
+		final StoredMatrixTransformations storedMatrixTransformations1 = new StoredMatrixTransformations(true);
+		storedMatrixTransformations1.add(graphicsHolder -> graphicsHolder.translate(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5));
 
 		// has down button, has up button, pressed down button, pressed up button
 		final boolean[] buttonStates = {false, false, false, false};
+		final ObjectArrayList<ObjectObjectImmutablePair<BlockPos, Lift>> sortedPositionsAndLifts = new ObjectArrayList<>();
 
 		blockEntity.forEachTrackPosition(trackPosition -> {
-			renderLiftObjectLink(graphicsHolder, world, blockPos, trackPosition, facing, holdingLinker);
+			renderLiftObjectLink(storedMatrixTransformations1, world, blockPos, trackPosition, facing, holdingLinker);
 			BlockLiftButtons.hasButtonsClient(trackPosition, buttonStates, (floorIndex, lift) -> {
+				sortedPositionsAndLifts.add(new ObjectObjectImmutablePair<>(trackPosition, lift));
 				final ObjectArraySet<LiftDirection> instructionDirections = lift.hasInstruction(floorIndex);
 				instructionDirections.forEach(liftDirection -> {
 					switch (liftDirection) {
@@ -64,6 +71,7 @@ public class RenderLiftButtons extends BlockEntityRenderer<BlockLiftButtons.Bloc
 			});
 		});
 
+		sortedPositionsAndLifts.sort(Comparator.comparingInt(sortedPositionAndLift -> blockPos.getManhattanDistance(new Vector3i(sortedPositionAndLift.left().data))));
 		final HitResult hitResult = MinecraftClient.getInstance().getCrosshairTargetMapped();
 		final boolean lookingAtTopHalf;
 		final boolean lookingAtBottomHalf;
@@ -78,34 +86,68 @@ public class RenderLiftButtons extends BlockEntityRenderer<BlockLiftButtons.Bloc
 			lookingAtBottomHalf = inBlock && (!buttonStates[1] || hitY < 0.25);
 		}
 
-		graphicsHolder.rotateYDegrees(-facing.asRotation());
-		graphicsHolder.translate(0, 0, 0.4375 - SMALL_OFFSET);
+		final StoredMatrixTransformations storedMatrixTransformations2 = storedMatrixTransformations1.copy();
+		storedMatrixTransformations2.add(graphicsHolder -> {
+			graphicsHolder.rotateYDegrees(-facing.asRotation());
+			graphicsHolder.translate(0, 0, 0.4375 - SMALL_OFFSET);
+		});
 
 		if (buttonStates[0]) {
-			graphicsHolder.createVertexConsumer(buttonStates[2] || lookingAtBottomHalf ? MoreRenderLayers.getLight(BUTTON_TEXTURE, true) : MoreRenderLayers.getExterior(BUTTON_TEXTURE));
-			IDrawing.drawTexture(graphicsHolder, -1.5F / 16, (buttonStates[1] ? 0.5F : 2.5F) / 16, 3F / 16, 3F / 16, 0, 0, 1, 1, facing, buttonStates[2] ? PRESSED_COLOR : lookingAtBottomHalf ? HOVER_COLOR : ARGB_GRAY, light);
+			RenderTrains.scheduleRender(BUTTON_TEXTURE, false, buttonStates[2] || lookingAtBottomHalf ? RenderTrains.QueuedRenderLayer.LIGHT_TRANSLUCENT : RenderTrains.QueuedRenderLayer.EXTERIOR, (graphicsHolder, offset) -> {
+				storedMatrixTransformations2.transform(graphicsHolder, offset);
+				IDrawing.drawTexture(graphicsHolder, -1.5F / 16, (buttonStates[1] ? 0.5F : 2.5F) / 16, 3F / 16, 3F / 16, 0, 0, 1, 1, facing, buttonStates[2] ? PRESSED_COLOR : lookingAtBottomHalf ? HOVER_COLOR : ARGB_GRAY, light);
+				graphicsHolder.pop();
+			});
 		}
 		if (buttonStates[1]) {
-			graphicsHolder.createVertexConsumer(buttonStates[3] || lookingAtTopHalf ? MoreRenderLayers.getLight(BUTTON_TEXTURE, true) : MoreRenderLayers.getExterior(BUTTON_TEXTURE));
-			IDrawing.drawTexture(graphicsHolder, -1.5F / 16, (buttonStates[0] ? 4.5F : 2.5F) / 16, 3F / 16, 3F / 16, 0, 1, 1, 0, facing, buttonStates[3] ? PRESSED_COLOR : lookingAtTopHalf ? HOVER_COLOR : ARGB_GRAY, light);
+			RenderTrains.scheduleRender(BUTTON_TEXTURE, false, buttonStates[3] || lookingAtTopHalf ? RenderTrains.QueuedRenderLayer.LIGHT_TRANSLUCENT : RenderTrains.QueuedRenderLayer.EXTERIOR, (graphicsHolder, offset) -> {
+				storedMatrixTransformations2.transform(graphicsHolder, offset);
+				IDrawing.drawTexture(graphicsHolder, -1.5F / 16, (buttonStates[0] ? 4.5F : 2.5F) / 16, 3F / 16, 3F / 16, 0, 1, 1, 0, facing, buttonStates[3] ? PRESSED_COLOR : lookingAtTopHalf ? HOVER_COLOR : ARGB_GRAY, light);
+				graphicsHolder.pop();
+			});
 		}
 
-		graphicsHolder.pop();
+		if (!sortedPositionsAndLifts.isEmpty()) {
+			final int count = Math.min(2, sortedPositionsAndLifts.size());
+			final float width = count == 1 ? 0.25F : 0.375F;
+
+			final StoredMatrixTransformations storedMatrixTransformations3 = storedMatrixTransformations2.copy();
+			storedMatrixTransformations3.add(graphicsHolder -> {
+				graphicsHolder.rotateZDegrees(180);
+				graphicsHolder.translate(-width / 2, 0, 0);
+			});
+
+			RenderTrains.scheduleRender(new Identifier(Init.MOD_ID, "textures/block/black.png"), false, RenderTrains.QueuedRenderLayer.EXTERIOR, (graphicsHolder, offset) -> {
+				storedMatrixTransformations3.transform(graphicsHolder, offset);
+				IDrawing.drawTexture(graphicsHolder, 0, -0.9375F, width, 0.40625F, Direction.UP, light);
+				graphicsHolder.pop();
+			});
+
+			for (int i = 0; i < count; i++) {
+				final double x = (i + 0.5) * width / count;
+				final StoredMatrixTransformations storedMatrixTransformations4 = storedMatrixTransformations3.copy();
+				storedMatrixTransformations4.add(graphicsHolder -> graphicsHolder.translate(x, -0.875, -SMALL_OFFSET));
+				RenderLifts.renderLiftDisplay(storedMatrixTransformations4, world, sortedPositionsAndLifts.get(i).right(), width / count, 0.3125F);
+			}
+		}
 	}
 
-	public static void renderLiftObjectLink(GraphicsHolder graphicsHolder, World world, BlockPos pos, BlockPos trackPosition, Direction facing, boolean holdingLinker) {
+	public static void renderLiftObjectLink(StoredMatrixTransformations storedMatrixTransformations, World world, BlockPos pos, BlockPos trackPosition, Direction facing, boolean holdingLinker) {
 		if (holdingLinker) {
 			final Direction trackFacing = IBlock.getStatePropertySafe(world, trackPosition, FACING);
-			graphicsHolder.createVertexConsumer(RenderLayer.getLines());
-			graphicsHolder.drawLineInWorld(
-					trackPosition.getX() - pos.getX() + trackFacing.getOffsetX() / 2F,
-					trackPosition.getY() - pos.getY() + 0.5F,
-					trackPosition.getZ() - pos.getZ() + trackFacing.getOffsetZ() / 2F,
-					facing.getOffsetX() / 2F,
-					0.25F,
-					facing.getOffsetZ() / 2F,
-					ARGB_WHITE
-			);
+			RenderTrains.scheduleRender(RenderTrains.QueuedRenderLayer.LINES, (graphicsHolder, offset) -> {
+				storedMatrixTransformations.transform(graphicsHolder, offset);
+				graphicsHolder.drawLineInWorld(
+						trackPosition.getX() - pos.getX() + trackFacing.getOffsetX() / 2F,
+						trackPosition.getY() - pos.getY() + 0.5F,
+						trackPosition.getZ() - pos.getZ() + trackFacing.getOffsetZ() / 2F,
+						facing.getOffsetX() / 2F,
+						0.25F,
+						facing.getOffsetZ() / 2F,
+						ARGB_WHITE
+				);
+				graphicsHolder.pop();
+			});
 		}
 	}
 }
--- fabric/src/main/java/org/mtr/mod/render/RenderLifts.java
+++ fabric/src/main/java/org/mtr/mod/render/RenderLifts.java
@@ -1,18 +1,23 @@
 package org.mtr.mod.render;
 
+import org.mtr.core.data.Lift;
+import org.mtr.core.data.LiftDirection;
 import org.mtr.core.data.LiftFloor;
 import org.mtr.core.data.Position;
 import org.mtr.core.tool.Vector;
 import org.mtr.mapping.holder.*;
 import org.mtr.mod.Init;
 import org.mtr.mod.Items;
+import org.mtr.mod.block.BlockLiftTrackFloor;
 import org.mtr.mod.client.ClientData;
+import org.mtr.mod.client.IDrawing;
 import org.mtr.mod.data.IGui;
 import org.mtr.mod.model.ModelLift1;
 import org.mtr.mod.model.ModelSmallCube;
 
 public class RenderLifts implements IGui {
 
+	private static final int LIFT_DISPLAY_COLOR = 0xFFFF0000;
 	private static final Identifier LIFT_TEXTURE = new Identifier("mtr:textures/vehicle/lift_1.png");
 	private static final ModelSmallCube MODEL_SMALL_CUBE = new ModelSmallCube(new Identifier("textures/block/redstone_block.png"));
 
@@ -73,6 +78,45 @@ public class RenderLifts implements IGui {
 					lift.getDoorValue() * 2, lift.getDoorValue() * 2, false,
 					0, 1, true, true, false, true, false
 			);
+
+			for (int i = 0; i < (lift.getIsDoubleSided() ? 2 : 1); i++) {
+				final boolean shouldRotate = i == 0;
+				final StoredMatrixTransformations storedMatrixTransformationsNew = storedMatrixTransformations.copy();
+				storedMatrixTransformationsNew.add(graphicsHolder -> {
+					if (shouldRotate) {
+						graphicsHolder.rotateYDegrees(180);
+					}
+					graphicsHolder.translate(0.875F, -1.5, lift.getDepth() / 2 - 0.25 - SMALL_OFFSET);
+				});
+				renderLiftDisplay(storedMatrixTransformationsNew, new World(clientWorld.data), lift, 0.1875F, 0.3125F);
+			}
 		});
+	}
+
+	public static void renderLiftDisplay(StoredMatrixTransformations storedMatrixTransformations, World world, Lift lift, float width, float height) {
+		final LiftDirection liftDirection = lift.getDirection();
+		final LiftFloor liftFloor = lift.getCurrentFloor();
+		final BlockEntity floorEntity = world.getBlockEntity(Init.positionToBlockPos(liftFloor.getPosition()));
+		final String floorNumber;
+
+		if (floorEntity != null && floorEntity.data instanceof BlockLiftTrackFloor.BlockEntity) {
+			floorNumber = ((BlockLiftTrackFloor.BlockEntity) floorEntity.data).getFloorNumber();
+		} else {
+			floorNumber = liftFloor.getNumber();
+		}
+
+		RenderTrains.scheduleRender(RenderTrains.QueuedRenderLayer.LIGHT_TRANSLUCENT, (graphicsHolder, offset) -> {
+			storedMatrixTransformations.transform(graphicsHolder, offset);
+			IDrawing.drawStringWithFont(graphicsHolder, floorNumber, IGui.HorizontalAlignment.CENTER, VerticalAlignment.BOTTOM, 0, height, width, -1, 18 / width, LIFT_DISPLAY_COLOR, false, MAX_LIGHT_GLOWING, null);
+			graphicsHolder.pop();
+		});
+
+		if (liftDirection != LiftDirection.NONE) {
+			RenderTrains.scheduleRender(new Identifier(Init.MOD_ID, "textures/block/sign/lift_arrow.png"), false, RenderTrains.QueuedRenderLayer.LIGHT_TRANSLUCENT, (graphicsHolder, offset) -> {
+				storedMatrixTransformations.transform(graphicsHolder, offset);
+				IDrawing.drawTexture(graphicsHolder, -width / 6, 0, width / 3, width / 3, 0, liftDirection == LiftDirection.UP ? 0 : 1, 1, liftDirection == LiftDirection.UP ? 1 : 0, Direction.UP, LIFT_DISPLAY_COLOR, MAX_LIGHT_GLOWING);
+				graphicsHolder.pop();
+			});
+		}
 	}
 }
