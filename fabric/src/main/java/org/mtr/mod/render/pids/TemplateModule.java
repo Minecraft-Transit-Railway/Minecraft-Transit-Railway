8c226344cf18a624d840c7080b72465218b244f3
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2024-05-10 21:26:44 -0700

    Add In-Game time to Template Module

--- fabric/src/main/java/org/mtr/mod/render/pids/TemplateModule.java
+++ fabric/src/main/java/org/mtr/mod/render/pids/TemplateModule.java
@@ -6,6 +6,7 @@ import org.mtr.core.tool.Utilities;
 import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectList;
 import org.mtr.mapping.holder.BlockPos;
 import org.mtr.mapping.holder.Direction;
+import org.mtr.mapping.holder.MinecraftClient;
 import org.mtr.mapping.mapper.GraphicsHolder;
 import org.mtr.mod.InitClient;
 import org.mtr.mod.block.BlockPIDSBase;
@@ -27,27 +28,48 @@ public class TemplateModule extends TextModule {
             return;
         }
 
-        ZonedDateTime date = ZonedDateTime.now();
-        int time = (int) ((date.toEpochSecond() + date.getOffset().getTotalSeconds()) % 86400);
-        String ampm = time >= 43200 ? "PM" : "AM";
-        int seconds = (int) Math.floor(time % 60F);
-        int minutes = (int) Math.floor(time % 3600 / 60F);
-        int hours = (int) Math.floor(time / 3600F);
-        int hours12 = hours % 12;
-        hours12 = hours12 == 0 ? 12 : hours12;
-
         String text = template
-                .replaceAll("\\$s", InitClient.findStation(blockPos).getName())
-                .replaceAll("\\$d", arrivalResponse.getDestination())
-                .replaceAll("\\$p", arrivalResponse.getPlatformName())
-                .replaceAll("\\$l", String.valueOf(arrivalResponse.getCarCount()))
-                .replaceAll("\\$n", arrivalResponse.getRouteName())
-                //Time Identifiers
+            .replaceAll("\\$s", InitClient.findStation(blockPos).getName())
+            .replaceAll("\\$d", arrivalResponse.getDestination())
+            .replaceAll("\\$p", arrivalResponse.getPlatformName())
+            .replaceAll("\\$l", String.valueOf(arrivalResponse.getCarCount()))
+            .replaceAll("\\$n", arrivalResponse.getRouteName());
+
+        //Time Identifiers
+        if (template.contains("$t")) {
+            ZonedDateTime date = ZonedDateTime.now();
+            int time = (int) ((date.toEpochSecond() + date.getOffset().getTotalSeconds()) % 86400);
+            String ampm = time >= 43200 ? "PM" : "AM";
+            int seconds = (int) Math.floor(time % 60F);
+            int minutes = (int) Math.floor(time % 3600 / 60F);
+            int hours = (int) Math.floor(time / 3600F);
+            int hours12 = hours % 12;
+            hours12 = hours12 == 0 ? 12 : hours12;
+            text = text
                 .replaceAll("\\$ts", String.format("%02d", seconds))
                 .replaceAll("\\$tm", String.format("%02d", minutes))
                 .replaceAll("\\$th2", String.valueOf(hours12))
                 .replaceAll("\\$th4", String.valueOf(hours))
                 .replaceAll("\\$tap", ampm);
+        }
+
+        //Game Time Identifiers
+        if (template.contains("$tg")) {
+            int time = (int) MinecraftClient.getInstance().getWorldMapped().getTime();
+            time = ((int) Math.floor(time * 72F / 20) + 21600) % 86400;
+            String ampm = time >= 43200 ? "PM" : "AM";
+            int seconds = (int) Math.floor(time % 60F);
+            int minutes = (int) Math.floor(time % 3600 / 60F);
+            int hours = (int) Math.floor(time / 3600F);
+            int hours12 = hours % 12;
+            hours12 = hours12 == 0 ? 12 : hours12;
+            text = text
+                .replaceAll("\\$tgs", String.format("%02d", seconds))
+                .replaceAll("\\$tgm", String.format("%02d", minutes))
+                .replaceAll("\\$tgh2", String.valueOf(hours12))
+                .replaceAll("\\$tgh4", String.valueOf(hours))
+                .replaceAll("\\$tgap", ampm);
+        }
 
         super.render(graphicsHolder, arrivals, renderPIDS, entity, blockPos, facing, text);
     }
