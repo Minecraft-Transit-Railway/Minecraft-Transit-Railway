9165a52c33a6e2de9b4723323af3778f2dde8b0d
Jonathan Ho <jonafanho@gmail.com>
2024-06-13 23:32:56 -0700

    Fixed common Config class using clientside code

--- fabric/src/main/java/org/mtr/mod/config/Config.java
+++ fabric/src/main/java/org/mtr/mod/config/Config.java
@@ -4,13 +4,15 @@ import org.apache.commons.io.FileUtils;
 import org.mtr.core.serializer.JsonReader;
 import org.mtr.core.serializer.ReaderBase;
 import org.mtr.core.tool.Utilities;
+import org.mtr.libraries.com.google.gson.JsonElement;
 import org.mtr.libraries.com.google.gson.JsonObject;
+import org.mtr.libraries.com.google.gson.JsonParser;
 import org.mtr.mod.Init;
-import org.mtr.mod.client.CustomResourceLoader;
 import org.mtr.mod.generated.config.ConfigSchema;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,12 +26,12 @@ public final class Config extends ConfigSchema {
 
 	public static void init(File baseFolder) {
 		if (instance == null || basePath == null) {
+			basePath = baseFolder.toPath();
 			try (final InputStream inputStream = Files.newInputStream(getConfigFilePath(), StandardOpenOption.READ)) {
-				instance = new Config(new JsonReader(CustomResourceLoader.readResource(inputStream)));
+				instance = new Config(new JsonReader(readResource(inputStream)));
 			} catch (Exception ignored) {
 				instance = new Config(new JsonReader(new JsonObject()));
 			}
-			basePath = baseFolder.toPath();
 		}
 		save();
 	}
@@ -37,6 +39,15 @@ public final class Config extends ConfigSchema {
 	private Config(ReaderBase readerBase) {
 		super(readerBase);
 		updateData(readerBase);
+	}
+
+	public static JsonElement readResource(InputStream inputStream) {
+		try (final InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+			return JsonParser.parseReader(inputStreamReader);
+		} catch (Exception e) {
+			Init.LOGGER.error("", e);
+			return new JsonObject();
+		}
 	}
 
 	public static Client getClient() {
