c197f1e5d22fccaa111b6a254f93f92756506837
LX862 <kenny.mh.hui@outlook.com>
2024-06-25 20:06:39 +0800

    Make PSD/APG detect redstone

--- fabric/src/main/java/org/mtr/mod/block/BlockPSDAPGDoorBase.java
+++ fabric/src/main/java/org/mtr/mod/block/BlockPSDAPGDoorBase.java
@@ -65,7 +65,7 @@ public abstract class BlockPSDAPGDoorBase extends BlockPSDAPGBase implements Blo
 	public VoxelShape getCollisionShape2(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
 		// The serverside collision shape is always empty, and the clientside collision shape is determined by the vehicle door positions the client sees
 		final BlockEntity entity = world.getBlockEntity(pos);
-		if (entity != null && entity.data instanceof BlockEntityBase && entity.getWorld().isClient() && ((BlockEntityBase) entity.data).doorValue == 0) {
+		if (entity != null && entity.data instanceof BlockEntityBase && entity.getWorld().isClient() && ((BlockEntityBase) entity.data).getDoorValue() == 0) {
 			return super.getCollisionShape2(state, world, pos, context);
 		} else {
 			return VoxelShapes.empty();
@@ -108,8 +108,9 @@ public abstract class BlockPSDAPGDoorBase extends BlockPSDAPGBase implements Blo
 	}
 
 	public static abstract class BlockEntityBase extends BlockEntityExtension implements IGui {
-
+		private static int REDSTONE_DETECT_DEPTH = 2;
 		private double doorValue;
+		private double redstoneDoorValue;
 
 		public BlockEntityBase(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 			super(type, pos, state);
@@ -120,7 +121,35 @@ public abstract class BlockPSDAPGDoorBase extends BlockPSDAPGBase implements Blo
 		}
 
 		public double getDoorValue() {
-			return doorValue;
+			return Math.max(doorValue, redstoneDoorValue);
+		}
+
+		public void updateRedstone(float tickDelta) {
+			World world = getWorldMapped();
+			double delta = (tickDelta / 20) / 2;
+
+			if(world != null && receivedRedstonePower(world, getPos2(), getCachedState2())) {
+				redstoneDoorValue = Utilities.clamp(redstoneDoorValue + delta, 0, 1);
+			} else {
+				redstoneDoorValue = Utilities.clamp(redstoneDoorValue - delta, 0, 1);
+			}
+		}
+
+		private boolean receivedRedstonePower(World world, BlockPos pos, BlockState state) {
+			DoubleBlockHalf half = IBlock.getStatePropertySafe(state, HALF);
+			Direction facing = IBlock.getStatePropertySafe(state, FACING);
+			EnumSide side = IBlock.getStatePropertySafe(state, SIDE);
+			Direction otherDirection = side == EnumSide.LEFT ? facing.rotateYClockwise() : facing.rotateYCounterclockwise();
+			BlockPos platformPos = (half == DoubleBlockHalf.UPPER) ? pos.down(2) : pos.down(1);
+
+			for(int i = 0; i < REDSTONE_DETECT_DEPTH; i++) {
+				BlockPos checkPos = platformPos.offset(Direction.DOWN, i + 1);
+				boolean emit = world.isEmittingRedstonePower(checkPos, Direction.UP);
+				boolean emitNearby = world.isEmittingRedstonePower(checkPos.offset(otherDirection), Direction.UP);
+				if(emit || emitNearby) return true;
+			}
+
+			return false;
 		}
 	}
 }
