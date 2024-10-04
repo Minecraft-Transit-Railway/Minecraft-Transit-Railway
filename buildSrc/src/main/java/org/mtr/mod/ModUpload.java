094e35a8f65dc2261919e7c2aa79781d4adf6fd3
Jonathan Ho <jonafanho@gmail.com>
2024-10-03 17:22:43 -0700

    Fixed mod upload script dependencies

--- buildSrc/src/main/java/org/mtr/mod/ModUpload.java
+++ buildSrc/src/main/java/org/mtr/mod/ModUpload.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class ModUpload {
 
@@ -19,17 +20,23 @@ public class ModUpload {
 			for (final String minecraftVersion : MINECRAFT_VERSIONS) {
 				for (final ModLoader modLoader : ModLoader.values()) {
 					final String modVersion = String.format("%s-%s+%s", modLoader.name, args[0], minecraftVersion);
-					final String modVersionUpperCase = modVersion.toUpperCase(Locale.ENGLISH).replace("HOTFIX", "hotfix");
+					final String modVersionUpperCase = String.format("%s-%s+%s", modLoader.name.toUpperCase(Locale.ENGLISH), args[0], minecraftVersion);
 					final String fileName = String.format("MTR-%s.jar", modVersion);
 					final Path filePath = Paths.get("build/release").resolve(fileName);
 					final ReleaseStatus releaseStatus = args[0].toLowerCase(Locale.ENGLISH).contains("beta") ? ReleaseStatus.BETA : ReleaseStatus.RELEASE;
+
+					final Map<String, DependencyType> dependenciesCurseForge = new HashMap<>();
+					if (modLoader == ModLoader.FABRIC) {
+						dependenciesCurseForge.put("fabric-api", DependencyType.REQUIRED);
+					}
+					dependenciesCurseForge.put("the-tube", DependencyType.OPTIONAL);
 
 					do {
 					} while (!new ModId("266707", ModProvider.CURSE_FORGE).uploadFile(
 							"",
 							modVersionUpperCase,
 							"See Discord",
-							modLoader == ModLoader.FABRIC ? Collections.singletonMap("fabric-api", DependencyType.REQUIRED) : new HashMap<>(),
+							dependenciesCurseForge,
 							releaseStatus,
 							Collections.singleton(minecraftVersion),
 							Collections.singleton(modLoader),
@@ -39,12 +46,18 @@ public class ModUpload {
 							args[1]
 					));
 
+					final Map<String, DependencyType> dependenciesModrinth = new HashMap<>();
+					if (modLoader == ModLoader.FABRIC) {
+						dependenciesModrinth.put("P7dR8mSH", DependencyType.REQUIRED);
+					}
+					dependenciesModrinth.put("8pXSjgW8", DependencyType.INCOMPATIBLE);
+
 					do {
 					} while (!new ModId("XKPAmI6u", ModProvider.MODRINTH).uploadFile(
 							modVersionUpperCase,
 							modVersionUpperCase,
 							"See Discord",
-							modLoader == ModLoader.FABRIC ? Collections.singletonMap("P7dR8mSH", DependencyType.REQUIRED) : new HashMap<>(),
+							dependenciesModrinth,
 							releaseStatus,
 							Collections.singleton(minecraftVersion),
 							Collections.singleton(modLoader),
