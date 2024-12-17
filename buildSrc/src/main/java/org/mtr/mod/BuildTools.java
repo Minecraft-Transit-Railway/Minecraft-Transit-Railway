cc8b5ecb81fcb9cc4837ac1b6dca499bfa22976c
Jonathan Ho <jonafanho@gmail.com>
2024-12-17 14:13:34 -0800

    Bump version and fixed Crowdin API key

--- buildSrc/src/main/java/org/mtr/mod/BuildTools.java
+++ buildSrc/src/main/java/org/mtr/mod/BuildTools.java
@@ -132,7 +132,7 @@ public class BuildTools {
 				try (final ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
 					ZipEntry zipEntry;
 					while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-						FileUtils.write(path.resolve("src/main/resources/assets/mtr/lang").resolve(zipEntry.getName()).toFile(), IOUtils.toString(zipInputStream, StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+						FileUtils.write(path.resolve("src/main/resources/assets/mtr/lang").resolve(zipEntry.getName().toLowerCase(Locale.ENGLISH)).toFile(), IOUtils.toString(zipInputStream, StandardCharsets.UTF_8), StandardCharsets.UTF_8);
 						zipInputStream.closeEntry();
 					}
 				}
