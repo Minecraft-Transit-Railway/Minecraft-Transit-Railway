549fbe9162bab3f21c500783ca50fac6c875b53f
xiangao0904 <3150399699@qq.com>
2026-05-18 18:53:25 +0000

    Fix GPU instancing draw state and matrix packing

--- fabric/src/main/java/org/mtr/mod/render/GpuObjRenderer.java
+++ fabric/src/main/java/org/mtr/mod/render/GpuObjRenderer.java
@@ -7,11 +7,13 @@ import org.mtr.libraries.it.unimi.dsi.fastutil.ints.IntArrayList;
 import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.mtr.libraries.it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.mtr.mapping.holder.Vector3d;
+import org.mtr.mapping.mapper.GraphicsHolder;
 import org.mtr.mapping.render.batch.MaterialProperties;
 import org.mtr.mapping.render.object.VertexArray;
 import org.mtr.mapping.render.object.VertexBuffer;
 import org.mtr.mapping.render.shader.ShaderManager;
 import org.mtr.mapping.render.vertex.VertexAttributeMapping;
+import org.mtr.mapping.render.vertex.VertexAttributeState;
 import org.mtr.mapping.render.vertex.VertexAttributeSource;
 import org.mtr.mapping.render.vertex.VertexAttributeType;
 import org.mtr.mod.data.IGui;
@@ -36,6 +38,7 @@ public final class GpuObjRenderer implements IGui {
 	private static final int TRANSLATION_X_BYTE_OFFSET = 12 * Float.BYTES;
 	private static final int TRANSLATION_Y_BYTE_OFFSET = 13 * Float.BYTES;
 	private static final int TRANSLATION_Z_BYTE_OFFSET = 14 * Float.BYTES;
+	private static final VertexAttributeState DEFAULT_DRAW_STATE = new VertexAttributeState(ARGB_WHITE, GraphicsHolder.getDefaultLight(), org.mtr.mapping.render.tool.Utilities.create());
 
 	private final ShaderManager shaderManager = new ShaderManager();
 	private final VertexBuffer instanceBuffer = new VertexBuffer();
@@ -68,9 +71,22 @@ public final class GpuObjRenderer implements IGui {
 		}
 
 		scratchInstanceBuffer.clear();
-		for (int i = 0; i < MATRIX_FLOATS; i++) {
-			scratchInstanceBuffer.putFloat(matrix.get(i / 4, i % 4));
-		}
+		scratchInstanceBuffer.putFloat(matrix.m00());
+		scratchInstanceBuffer.putFloat(matrix.m01());
+		scratchInstanceBuffer.putFloat(matrix.m02());
+		scratchInstanceBuffer.putFloat(matrix.m03());
+		scratchInstanceBuffer.putFloat(matrix.m10());
+		scratchInstanceBuffer.putFloat(matrix.m11());
+		scratchInstanceBuffer.putFloat(matrix.m12());
+		scratchInstanceBuffer.putFloat(matrix.m13());
+		scratchInstanceBuffer.putFloat(matrix.m20());
+		scratchInstanceBuffer.putFloat(matrix.m21());
+		scratchInstanceBuffer.putFloat(matrix.m22());
+		scratchInstanceBuffer.putFloat(matrix.m23());
+		scratchInstanceBuffer.putFloat(matrix.m30());
+		scratchInstanceBuffer.putFloat(matrix.m31());
+		scratchInstanceBuffer.putFloat(matrix.m32());
+		scratchInstanceBuffer.putFloat(matrix.m33());
 		scratchInstanceBuffer.putInt(packedColor);
 		scratchInstanceBuffer.putInt(packedLight);
 		meshEntry.addInstance(scratchInstanceData, useDefaultOffset);
@@ -137,6 +153,8 @@ public final class GpuObjRenderer implements IGui {
 		meshEntry.staticObjMesh.vertexArray.bind();
 		instanceBuffer.bind(GL33.GL_ARRAY_BUFFER);
 		instanceBuffer.upload(byteBuffer, VertexBuffer.USAGE_STREAM_DRAW);
+		DEFAULT_DRAW_STATE.apply();
+		meshEntry.staticObjMesh.vertexArray.materialProperties.vertexAttributeState.apply();
 		materialProperties.vertexAttributeState.apply();
 		GL33.glDrawElementsInstanced(GL33.GL_TRIANGLES, meshEntry.staticObjMesh.vertexArray.indexBuffer.getVertexCount(), meshEntry.staticObjMesh.vertexArray.indexBuffer.indexType, 0, instanceCount);
 		GpuObjDebugStats.recordDrawInstanced();
